121929af541d196d2cc8afd099ffb3037c6a8d0b
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-12-28 16:29:26 +0700

    Add SHA512

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -1,5 +1,6 @@
 package web;
 
+import java.security.*;
 import org.hibernate.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +23,7 @@ public class Main {
 	String saveUser(String email, String password, String name) {
 		Member m = new Member();
 		m.email = email;
-		m.password = password;
+		m.password = sha512(password);
 		m.name = name;
 		Session s = factory.openSession();
 		s.save(m);
@@ -31,4 +32,21 @@ public class Main {
 	
 	@Autowired
 	SessionFactory factory;
+
+	public String sha512(String password){
+		String generatedPassword = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			byte[] bytes = md.digest(password.getBytes("UTF-8"));
+			StringBuilder sb = new StringBuilder();
+			for (int i=0; i< bytes.length ;i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
+					.substring(1));
+			}
+			generatedPassword = sb.toString();
+		} 
+		catch (Exception e) { }
+		return generatedPassword;
+	}
+	
 }
