ff0e53df9423225e71204aeea394927a94c67644
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-12-28 14:59:34 +0700

    Update

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -1,1 +1,15 @@
+package web;
 
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class Main {
+
+	@RequestMapping("/test") @ResponseBody
+	int test() {
+		return 555;
+	}
+	
+	
+}
