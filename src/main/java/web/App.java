aed0b4ab4c1a684907b1a861b4f0f2df6fe9f406
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-12-28 14:55:14 +0700

    Add App.java

--- src/main/java/web/App.java
+++ src/main/java/web/App.java
@@ -1,1 +1,11 @@
+package web;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class App {
+	public static void main(String [ ] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
