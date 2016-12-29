d1c0662d0197989d7cdadcb5a456dafee8cb953b
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-12-29 14:27:13 +0700

    Add Topic table

--- schema.sql
+++ schema.sql
@@ -3,9 +3,16 @@ create database spring default charset='utf8';
 grant all on spring.* to spring@'%';
 use spring;
 create table member(
-  code  serial,
-  email varchar(255) unique,
+  code     serial,
+  email    varchar(255) unique,
   password varchar(2047),
-  name  varchar(255)
+  name     varchar(255)
 );
 
+create table topic(
+  code   serial,
+  title  varchar(2047),
+  detail varchar(16383),
+  photo  varchar(255),
+  user   bigint
+);
